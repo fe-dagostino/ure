d920455baa4edcf48e15c43be997b6740a4b759a
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2024-10-19 22:31:00 +0200

    add export for both SRC and BIN to parent

--- cmake/libsigc++.cmake
+++ cmake/libsigc++.cmake
@@ -7,4 +7,13 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(sigc)
 
+message( INFO " SIGC++ SRC DIR: [${sigc_SOURCE_DIR}]" )
+message( INFO " SIGC++ BIN DIR: [${sigc_BINARY_DIR}]" )
+
 include_directories( ${sigc_SOURCE_DIR} ${sigc_BINARY_DIR} )
+
+if(hasParent)
+  # make both ${sigc_SOURCE_DIR} and ${sigc_BINARY_DIR} visible at parent scope.
+  set( sigc_SOURCE_DIR ${sigc_SOURCE_DIR} PARENT_SCOPE )
+  set( sigc_BINARY_DIR ${sigc_BINARY_DIR} PARENT_SCOPE )
+endif(hasParent)
