8605b961f0399d33b4cd188eda84c57e7d99ea60
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2024-10-19 17:49:02 +0200

    udpate cmakelists for glfw

--- cmake/glfw3.cmake
+++ cmake/glfw3.cmake
@@ -2,15 +2,17 @@
 set(GLFW_VERSION 3.4)
 
 FetchContent_Declare(
-                      glfw3
+                      glfw
                       GIT_REPOSITORY https://github.com/glfw/glfw.git
                       GIT_TAG master
                     )
 
-set(GLFW_BUILD_DOCS     OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_TESTS    OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-
-FetchContent_MakeAvailable(glfw3)
-
-include_directories( ${GLFW_SOURCE_DIR}/include )
+set(GLFW_BUILD_DOCS        OFF CACHE INTERNAL "" )
+set(GLFW_BUILD_TESTS       OFF CACHE INTERNAL "" )
+set(GLFW_BUILD_EXAMPLES    OFF CACHE INTERNAL "" )
+set(GLFW_BUILD_WAYLAND      ON CACHE INTERNAL "" )
+set(GLFW_BUILD_X11          ON CACHE INTERNAL "" )
+set(GLFW_LIBRARY_TYPE   STATIC CACHE INTERNAL "" )
+set(GLFW_INSTALL           OFF CACHE INTERNAL "" )
+                    
+FetchContent_MakeAvailable(glfw)
