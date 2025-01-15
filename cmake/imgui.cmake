b6aa1caa5f7ce52d3e7b97b66929dfac9235d9fe
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2025-01-15 01:24:56 +0100

    defined cmake for Dear ImGui

---
+++ cmake/imgui.cmake
@@ -0,0 +1,31 @@
+
+FetchContent_Declare(
+  ImGui
+  GIT_REPOSITORY    https://github.com/ocornut/imgui.git
+  GIT_TAG           master # master
+  GIT_SHALLOW       TRUE
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND     ""
+)
+
+FetchContent_MakeAvailable(ImGui)
+
+if ( ENABLE_GLFW )
+  message( INFO " EXPORT ImGui SRC DIR: [${imgui_SOURCE_DIR}]" )
+  set ( IMGUI_SRC   ${imgui_SOURCE_DIR}/imgui.cpp 
+                    ${imgui_SOURCE_DIR}/imgui_demo.cpp 
+                    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+                    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+                    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+  )
+ 
+endif()
+
+if ( ENABLE_GLES OR ENABLE_OGL3 )
+  set ( IMGUI_BACKEND_SRC   ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp 
+                            ${imgui_SOURCE_DIR}/backends/imgui_impl_opengl3.cpp
+  )
+endif()
+
+add_library( ImGui   ${IMGUI_SRC} ${IMGUI_BACKEND_SRC} )
+include_directories( ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends )
